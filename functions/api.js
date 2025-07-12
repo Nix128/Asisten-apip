32f8f7d2a5fe96b36ba78d11c47f0dd78a6f5eeb
nixon <nixon128790@gmail.com>
2025-07-13 01:25:16 +0530

    Refactor: Restructure project into src directory for Netlify

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,5 @@
 const serverless = require('serverless-http');
-const app = require('../index');
+// Point to the main app file inside the src directory
+const app = require('../src/index');
 
 module.exports.handler = serverless(app);
